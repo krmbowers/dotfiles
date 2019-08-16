00da7db79bab5617f8a05403eecbed231579c02d
Kristin Bowers <42662574+krmbowers@users.noreply.github.com>
2019-08-16 13:21:35 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -3,23 +3,18 @@
 # Install Brew Packages
 brew install python
 brew install tree
+brew install tmux
+brew install grep
 
 # Install MacOS Applications
 brew cask install google-chrome
 brew cask install firefox
-brew cask install sublime-text
+brew cask install visual-studio-code
 brew cask install virtualbox
 brew cask install sourcetree
 brew cask install spotify
-brew cask install discord
-brew cask install google-backup-and-sync
-brew cask install skype
-brew cask install gimp
 brew cask install vlc
 brew cask install hyperdock
-brew cask install divvy
-
-# Install Source Code Pro Font
-brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro
+brew cask install docker
+brew cask install slack
 
